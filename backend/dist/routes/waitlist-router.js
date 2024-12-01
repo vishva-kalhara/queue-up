b7f7d39662b6269d518ea7431df553e5aff8f6f6
Wishva Kalhara <vishvakalhara@gmail.com>
2024-12-01 20:30:39 +0530

    removed dist

--- backend/dist/routes/waitlist-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const waitlist_controller_1 = require("../controllers/waitlist-controller");
-const waitlistRouter = (0, express_1.Router)({ mergeParams: true });
-waitlistRouter.route("/").post(waitlist_controller_1.addUserToWaitlist);
-// waitlistRouter.use(protect);
-waitlistRouter.route("/").get(waitlist_controller_1.getAppWaitlistData);
-exports.default = waitlistRouter;
